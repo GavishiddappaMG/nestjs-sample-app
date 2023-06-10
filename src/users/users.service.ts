9dfa85074e8b67e0adbf9869645c9e23e2226dc9
GavishiddappaMG <gmgadagi@gmail.com>
2023-06-10 14:30:04 +0530

    feat: create and save user data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -13,7 +13,7 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return this.repo.findOne({ where: { id } });
+    return this.repo.findOneBy({ id });
   }
 
   find(email: string) {
@@ -21,18 +21,18 @@ export class UsersService {
   }
 
   async update(id: number, attrs: Partial<User>) {
-    const user = await this.repo.findOne({ where: { id } });
+    const user = await this.repo.findOneBy({ id });
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
   }
 
   async remove(id: number) {
-    const user = await this.repo.findOne({ where: { id } });
+    const user = await this.repo.findOneBy({ id });
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     return this.repo.remove(user);
   }
